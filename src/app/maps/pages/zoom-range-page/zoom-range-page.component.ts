9d2ba3831321430fc09fdcd0c62600da3d1904ed
Heider Rey Hernandez <heiderhrnndz@gmail.com>
2023-07-13 18:19:05 -0500

    Mapa renderizado

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
+  
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-75.56768727196558, 6.239536372223171);
+
+  ngAfterViewInit(): void {
+    
+    if(!this.divMap) throw 'El elemento HTML no fue encontrado'
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+      });
+
+      this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+
+  mapListeners() {
+    if(!this.map) throw 'Mapa no inicializado'
+
+    this.map.on('zoom', (event) => {
+      this.zoom = this.map!.getZoom();
+    })
+
+    this.map.on('zoomend', (event) => {
+      if(this.map!.getZoom() < 18) return;
+      this.map?.zoomTo(18);
+    })
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+      
+    })
+  }
+
+  zoomIn(){
+    this.map?.zoomIn();
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string){
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 
 }
+
