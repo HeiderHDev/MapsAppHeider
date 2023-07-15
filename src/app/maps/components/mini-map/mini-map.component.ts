9b86beb7afcf2ebbf20d12ddbef3d7a908efd7ab
Heider Rey Hernandez <heiderhrnndz@gmail.com>
2023-07-15 17:22:19 -0500

    Proyecto terminado con el minimapa

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
+
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
 export class MiniMapComponent {
+
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if(!this.divMap?.nativeElement) throw 'El elemento HTML no fue encontrado'
+    if(!this.lngLat) throw 'LngLat no fue encontrado'
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false  
+    });
+
+      new Marker()
+        .setLngLat(this.lngLat)
+        .addTo(map);
+    
+  }
 
 }
