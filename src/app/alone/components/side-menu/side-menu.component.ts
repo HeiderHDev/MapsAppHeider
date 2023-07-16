ff36e97dbdd8f9c1029d8a9fd466734327757554
Heider Rey Hernandez <heiderhrnndz@gmail.com>
2023-07-15 22:56:34 -0500

    Proyecto terminado

--- src/app/alone/components/side-menu/side-menu.component.ts
+++ src/app/alone/components/side-menu/side-menu.component.ts
@@ -1,20 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-interface MenuItem{
+interface MenuItem {
+  name : string;
   route: string;
-  name: string;
 }
 
+
 @Component({
-  selector: 'maps-side-menu',
+  standalone: true,
+  selector: 'side-menu',
+  imports: [ CommonModule, RouterModule ],
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
+
   public menuItems: MenuItem[] = [
     { route: '/maps/fullscreen', name: 'FullScreen' },
     { route: '/maps/zoom-range', name: 'ZoomRange' },
     { route: '/maps/markers', name: 'Markers' },
     { route: '/maps/properties', name: 'Houses' },
+    { route: '/alone', name: 'Alone Page' },
   ];
+
 }
